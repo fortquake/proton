c377ae077178cb1dc88189ea3b2f1fe243bf7761
Fort Quake <48767278+fortquake@users.noreply.github.com>
2023-06-20 11:20:10 +0100

    Update installer.py

--- installer.py
+++ installer.py
@@ -5,4 +5,4 @@ It will install:\n- pyautogui\n- pydirectinput\n- playsound\n- keyboard\n
 Press enter to start the installer.\nTo exit, close the window.''')
 input("> "); print("\nThe installer will proceed.\n")
 os.system("pip install keyboard"); os.system("py -m pip install pyautogui"); os.system("py -m pip install pydirectinput"); os.system("py -m pip install playsound==1.2.2")
-print("Proton Installer has completed setup, you may now run Proton."); t.sleep(9); exit()
+print("Proton Installer has completed setup, you may now run Proton.")
